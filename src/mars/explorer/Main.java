15ad44b370d1f7a967831924853c443649f8e0ac
Asgeir Sognefest <sogasg@gmail.com>
2013-11-06 13:36:43 +0100

    added main class

---
+++ src/mars/explorer/Main.java
@@ -0,0 +1,12 @@
+package mars.explorer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asgeir
+ * Date: 06.11.13
+ * Time: 13:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+}
